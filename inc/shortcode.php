1df630b682dc5278fe5a79d2461f56f5994f9084
Marek Melichar <koprivackej@gmail.com>
2017-06-25 22:15:52 +0200

    new design 25.6.

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -15,17 +15,20 @@ function xevoq_demo_shortcode( $atts ) {
   }
 
   // create the html
-  $html .= '<div id="demo">';
+  $html .= '<div id="demo" class="row">';
     $html .= '<ul class="'. esc_attr($atts['slideshow_class']) .'">';
   foreach ( $media as $img ) {
     $alt_text = get_post_meta($img->ID, '_wp_attachment_image_alt', true);
 
     $html .= '<li class="'. esc_attr( $atts['slide_class'] ) .'">';
     $html .= '<img src="'. esc_url( wp_get_attachment_image_url($img->ID, 'full') ) .'" alt="'. esc_attr( $img->post_title ) .'" >';
-    $html .= '<span class="icon-slide-'. esc_attr( $img->post_title ) .'-cursor"></span><span class="icon-slide-'. esc_attr( $img->post_title ) .' '. $alt_text .'"></span><span class="icon-slide-'. esc_attr( $img->post_title ) .'-text">'. esc_attr( $img->post_content ) .'</span>';
+    $html .= '<span class="icon-slide-'. esc_attr( $img->post_title ) .'-cursor"></span><span class="icon-slide-'. esc_attr( $img->post_title ) .'-text">'. esc_attr( $img->post_content ) .'</span>';
     $html .= '</li>';
   }
     $html .= '</ul>';
+    $html .= '<span class="nav_point_1"></span>';
+    $html .= '<span class="nav_point_2"></span>';
+    $html .= '<span class="nav_point_3"></span>';
     $html .= '<a href="/#demo" id="try-again-demo">Try it again</a>';
   $html .= '</div>';
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 (function($, window, document) {
 
+  var hasScrollbar = function() {
+    // The Modern solution
+    if (typeof window.innerWidth === 'number')
+        return window.innerWidth > document.documentElement.clientWidth
+
+    // rootElem for quirksmode
+    var rootElem = document.documentElement || document.body
+
+    // Check overflow style property on body for fauxscrollbars
+    var overflowStyle
+
+    if (typeof rootElem.currentStyle !== 'undefined')
+        overflowStyle = rootElem.currentStyle.overflow
+
+    overflowStyle = overflowStyle || window.getComputedStyle(rootElem, '').overflow
+
+    // Also need to check the Y axis overflow
+    var overflowYStyle
+
+    if (typeof rootElem.currentStyle !== 'undefined')
+        overflowYStyle = rootElem.currentStyle.overflowY
+
+    overflowYStyle = overflowYStyle || window.getComputedStyle(rootElem, '').overflowY
+
+    var contentOverflows = rootElem.scrollHeight > rootElem.clientHeight
+    var overflowShown    = /^(visible|auto)$/.test(overflowStyle) || /^(visible|auto)$/.test(overflowYStyle)
+    var alwaysShowScroll = overflowStyle === 'scroll' || overflowYStyle === 'scroll'
+
+    return (contentOverflows && overflowShown) || (alwaysShowScroll)
+  }
+
+
+  if (hasScrollbar()) {
+    $('html').addClass('has-scrollbar');
+  }
+
+
   var slideshow = $('.slideshow'),
       slides = $('.slide')
       index = 0,
@@ -8,7 +45,7 @@
       tryAgainBtn = $('#try-again-demo');
 
   while (index <= slides.length) {
-    trigger = $(`.icon-slide-${index++}-cursor`)
+    trigger = $(`.icon-slide-demo_${index++}-cursor`)
     triggersArray.push(trigger)
   }
 
